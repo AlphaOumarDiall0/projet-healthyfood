68d9acfe009fc9da0fcb1d914460aeb8e4abe8ef
AlphaOumarDiall0 <aod6268@gmail.com>
2024-08-03 22:25:01 +0000

    Premiere correction du script

--- js/index.js
+++ js/index.js
@@ -20,12 +20,12 @@ const currentYear = new Date().getFullYear();
 ///////////////////////////////////////////////////////////
 // Make mobile navigation work
 
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".header");
+// const btnNavEl = document.querySelector(".btn-mobile-nav");
+// const headerEl = document.querySelector(".header");
 
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
-});
+// btnNavEl.addEventListener("click", function () {
+//   headerEl.classList.toggle("nav-open");
+// });
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
